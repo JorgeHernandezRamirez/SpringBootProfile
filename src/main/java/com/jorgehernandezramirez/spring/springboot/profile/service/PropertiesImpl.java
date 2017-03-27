8684a7c6ee7a227cc1136478ddbcac3b469c8d25
VASS\jorge.hernandez <jorge.hernandez@vass.es>
2017-03-27 11:04:59 +0200

    no message

--- src/main/java/com/jorgehernandezramirez/spring/springboot/profile/service/PropertiesImpl.java
+++ src/main/java/com/jorgehernandezramirez/spring/springboot/profile/service/PropertiesImpl.java
@@ -1,9 +1,7 @@
 package com.jorgehernandezramirez.spring.springboot.profile.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Profile("!local")
